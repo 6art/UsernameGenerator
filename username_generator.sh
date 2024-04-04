27b99944a9cccb58861764e4eed60fa6fcc3a050
6art <119949084+6art@users.noreply.github.com>
2024-04-04 12:08:43 +0800

    Update username_generator.sh
    添加了位数范围询问

--- username_generator.sh
+++ username_generator.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-# 询问用户要创建的字典的位数
-echo "请输入你想要创建的字典的位数："
-read length
+# 询问用户要创建的字典的位数或位数范围
+echo "你想要创建特定位数的字典还是位数范围的字典？"
+echo "1. 特定位数"
+echo "2. 位数范围"
+read option
+
+if [ $option -eq 1 ]
+then
+    echo "请输入你想要创建的字典的位数："
+    read start
+    end=$start
+elif [ $option -eq 2 ]
+then
+    echo "请输入你想要创建的字典的位数范围（如：2-5）："
+    read range
+    start=${range%-*}
+    end=${range#*-}
+else
+    echo "无效的选择。"
+    exit 1
+fi
 
 # 提供一些快速模式
 echo "请选择一个模式："
@@ -18,7 +36,7 @@ read mode
 if [ $mode -eq 7 ]
 then
     charset=""
-    for ((i=1; i<=$length; i++))
+    for ((i=1; i<=$end; i++))
     do
         echo "第 $i 位是由什么构成？"
         echo "1. 数字"
@@ -84,13 +102,17 @@ else
             echo "无效的选择。"
             exit 1
     esac
+fi
+
+for ((length=$start; length<=$end; length++))
+do
     command="echo "
     for ((i=1; i<=$length; i++))
     do
         command+="$charset"
     done
-fi
+    command+=" | sed 's/ /\n/g' >> ${filename}_${start}-${end}位.txt"
+    eval $command
+done
 
-command+=" | sed 's/ /\n/g' > ${filename}_${length}位.txt"
-eval $command
-echo "字典已经生成在 ${filename}_${length}位.txt 文件中。"
+echo "字典已经生成在 ${filename}_${start}-${end}位.txt 文件中。"
