2b16909b0be0f8a57a056085296524460517dea3
6art <119949084+6art@users.noreply.github.com>
2024-04-04 16:04:34 +0800

    Update username_generator.sh

--- username_generator.sh
+++ username_generator.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-# 检查字典目录是否存在，如果不存在则创建
-if [ ! -d "字典" ]; then
-    mkdir 字典
-fi
-
-# 询问用户要创建的字典的位数或位数范围
-echo "你想要创建特定位数的字典还是位数范围的字典？"
+# 询问用户要创建的字典的位数
+echo "请输入你想要创建的字典的位数："
 echo "1. 特定位数"
 echo "2. 位数范围"
-read option
+read type
 
-if [ $option -eq 1 ]; then
+if [ $type -eq 1 ]
+then
     echo "请输入你想要创建的字典的位数："
-    read start
-    end=$start
-elif [ $option -eq 2 ]; then
-    echo "请输入你想要创建的字典的位数范围（如：2-5）："
-    read range
-    start=${range%-*}
-    end=${range#*-}
-else
-    echo "无效的选择。"
-    exit 1
+    read length
+    min_length=$length
+    max_length=$length
+elif [ $type -eq 2 ]
+then
+    echo "请输入你想要创建的字典的最小位数："
+    read min_length
+    echo "请输入你想要创建的字典的最大位数："
+    read max_length
 fi
 
 # 提供一些快速模式
@@ -36,9 +31,11 @@ echo "6. 所有字符均为大小写字母或数字"
 echo "7. 自定义模式"
 read mode
 
-charset=""
-if [ $mode -eq 7 ]; then
-    for ((i=1; i<=$end; i++)); do
+charset_array=()
+for ((i=1; i<=$max_length; i++))
+do
+    if [ $mode -eq 7 ]
+    then
         echo "第 $i 位是由什么构成？"
         echo "1. 数字"
         echo "2. 小写字母"
@@ -49,70 +46,66 @@ if [ $mode -eq 7 ]; then
         read choice
         case $choice in
             1)
-                charset+="0-9"
+                charset_array+=("{0..9}")
                 ;;
             2)
-                charset+="a-z"
+                charset_array+=("{a..z}")
                 ;;
             3)
-                charset+="A-Z"
+                charset_array+=("{A..Z}")
                 ;;
             4)
-                charset+="0-9a-z"
+                charset_array+=("{{0..9},{a..z}}")
                 ;;
             5)
-                charset+="0-9A-Z"
+                charset_array+=("{{0..9},{A..Z}}")
                 ;;
             6)
-                charset+="0-9a-zA-Z"
+                charset_array+=("{{0..9},{a..z},{A..Z}}")
                 ;;
             *)
                 echo "无效的选择。"
                 exit 1
         esac
-    done
-    echo "请输入你想要的文件名："
-    read filename
-else
-    case $mode in
-        1)
-            charset+="0-9"
-            filename="数字"
-            ;;
-        2)
-            charset+="a-z"
-            filename="小写字母"
-            ;;
-        3)
-            charset+="A-Z"
-            filename="大写字母"
-            ;;
-        4)
-            charset+="0-9a-z"
-            filename="小写字母或数字"
-            ;;
-        5)
-            charset+="0-9A-Z"
-            filename="大写字母或数字"
-            ;;
-        6)
-            charset+="0-9a-zA-Z"
-            filename="大小写字母或数字"
-            ;;
-        *)
-            echo "无效的选择。"
-            exit 1
-    esac
-fi
-
-for ((length=start; length<=end; length++)); do
-    command=""
-    for ((i=1; i<=length; i++)); do
-        command+="\$(echo -n \"$charset\" | shuf -n1)"
-    done
-    command+=" >> 字典/${filename}_${start}-${end}位.txt"
-    eval "echo $command"
-    eval $command
+    else
+        case $mode in
+            1)
+                charset_array+=("{0..9}")
+                ;;
+            2)
+                charset_array+=("{a..z}")
+                ;;
+            3)
+                charset_array+=("{A..Z}")
+                ;;
+            4)
+                charset_array+=("{{0..9},{a..z}}")
+                ;;
+            5)
+                charset_array+=("{{0..9},{A..Z}}")
+                ;;
+            6)
+                charset_array+=("{{0..9},{a..z},{A..Z}}")
+                ;;
+            *)
+                echo "无效的选择。"
+                exit 1
+        esac
+    fi
 done
 
-echo "字典已经生成在 字典/${filename}_${start}-${end}位.txt 文件中。"
+echo "请输入你想要的文件名："
+read filename
+
+mkdir -p 字典
+for ((n=$min_length; n<=$max_length; n++))
+do
+    command="echo "
+    for ((i=0; i<$n; i++))
+    do
+        command+="${charset_array[$i]}"
+    done
+    command+=" | sed 's/ /\n/g' > 字典/${filename}_${n}位.txt"
+    eval $command
+    echo "字典已经生成在 字典/${filename}_${n}位.txt 文件中。"
+done
